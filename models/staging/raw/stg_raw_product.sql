4fa023bd1c699621265c0108d217d95aae68a7e3
Isabel Thompson <isabel.loetzsch@googlemail.com>
2024-07-29 07:00:30 +0000

    creating first model for product

---
+++ models/staging/raw/stg_raw_product.sql
@@ -0,0 +1,18 @@
+with
+
+source as (
+
+    select * from {{ ('raw', 'product') }}
+
+);
+
+renamed as (
+
+    select
+        products_id
+        , purchase_price
+
+    from source
+)
+
+select * from renamed
